235ea18393ed34b238fcaccb3b0a9cedc914b25b
survivevivek <59173379+survivevivek@users.noreply.github.com>
2024-05-01 15:12:54 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,13 @@
+var firstName=`Vivek`;
+var mobileNumber=9003247144;
+var isAutomation = true;
+var isPlaywright;
+
+console.log(firstName);
+console.log(typeof firstName);
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+console.log(isAutomation);
+console.log(typeof isAutomation);
+console.log(isPlaywright);
+console.log(typeof isPlaywright);
---
+++ const_let_var.js
@@ -0,0 +1,17 @@
+const browserVersion = "Chrome";
+function getBrowserVersion(){
+    if(browserVersion=="Chrome"){
+        var browserVersion;
+    }
+    console.log(browserVersion);
+}
+getBrowserVersion();
+
+const browserVersion1 = "Chrome";
+function getBrowserVersion1(){
+    if(browserVersion1=="Chrome"){
+        let browserVersion;
+    }
+    console.log(browserVersion1);
+}
+getBrowserVersion1();
